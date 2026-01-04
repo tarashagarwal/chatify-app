91041cf9a826db34c3b283c9edcf67c27951c7cf
Tarash <tarashagarwal@gmail.com>
2026-01-03 18:23:49 -0600

    Ready for Deployment

--- backend/src/server.js
+++ backend/src/server.js
@@ -17,10 +17,12 @@ app.use('/api/message', messageRoutes);
 
 if (process.env.NODE_ENV === 'production') {
     app.use(express.static(path.join(__dirname, '../frontend/dist')));
-    app.get('*', (req, res) => {
+    app.get('*', (_, res) => {
         res.sendFile(path.resolve(__dirname, '../frontend','dist','index.html'));
     });
 }
+
+
 
 app.listen(3000, () => {
     console.log(`Server is running on port ${PORT}`);
