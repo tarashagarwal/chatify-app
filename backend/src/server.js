4645a1251d6283d2a5823704350a14ecf39d47f2
Tarash <tarashagarwal@gmail.com>
2026-01-03 18:05:11 -0600

    Adding React and Server inot one endpoint

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,16 +2,25 @@ import express from 'express';
 import dotenv from 'dotenv';
 import authRoutes from '../routes/auth.route.js';
 import messageRoutes from '../routes/message.route.js';
+import path from 'path';
 
 dotenv.config();
 const app = express();
 
 console.log(process.env.PORT);
-
+const __dirname = path.resolve();
 const PORT = process.env.PORT || 3000;
 
 app.use('/api/auth', authRoutes);
 app.use('/api/message', messageRoutes);
+
+
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '../frontend/dist')));
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, '../frontend','dist','index.html'));
+    });
+}
 
 app.listen(3000, () => {
     console.log(`Server is running on port ${PORT}`);
